ddad2dab01364a310a962b4dd27c8d91efc0fff6
jmanfredi <juanjmanfredi@gmail.com>
2019-01-04 18:54:55 -0800

    Added proof of work + validation

--- blockchain.py
+++ blockchain.py
@@ -4,14 +4,14 @@ from time import time
 
 
 class Blockchain(object):
-    def __init__(self):
-        self.chain = []
-        self.current_transactions = []
+  def __init__(self):
+    self.chain = []
+    self.current_transactions = []
 
         #Create genesis block
         self.new_block(previous_hash=1, proof=100)
         
-    def new_block(self, proof, previous_hash=None):
+        def new_block(self, proof, previous_hash=None):
         """
         Creates a new block in the blockchain
 
@@ -21,11 +21,11 @@ class Blockchain(object):
         """
 
         block = {
-            'index': len(self.chain) + 1,
-            'timestamp': time(),
-            'transactions': self.current_transactions,
-            'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        'index': len(self.chain) + 1,
+        'timestamp': time(),
+        'transactions': self.current_transactions,
+        'proof': proof,
+        'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
 
         #Reset the current list of transactions
@@ -33,40 +33,81 @@ class Blockchain(object):
 
         self.chain.append(block)
         return block
-    
-    def new_transaction(self, sender, recipient, amount):
-        """
-        Creates a new transaction to go into the next mined block
 
-        :param sender: <str> Address of the Sender
-        :param recipient: <str> Address of the Recipient
-        :param amount: <int> Amount
-        :return: <int> The index of the Block that will hold this transaction
-        """
+        def new_transaction(self, sender, recipient, amount):
+          """
+          Creates a new transaction to go into the next mined block
 
-        self.current_transactions.append({
+          :param sender: <str> Address of the Sender
+          :param recipient: <str> Address of the Recipient
+          :param amount: <int> Amount
+          :return: <int> The index of the Block that will hold this transaction
+          """
+
+          self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
             'amount': amount,
-        })
+            })
 
-        return self.last_block['index'] + 1
+          return self.last_block['index'] + 1
 
-    @property
-    def last_block(self):
-        #Returns the last block in the chain
-        return self.chain[-1]
-    
-    @staticmethod
-    def hash(block):
-        """
-        Creates an SHA-256 hash of a Block
+        @property
+        def last_block(self):
+          #Returns the last block in the chain
+          return self.chain[-1]
 
-        :param block: <dict> Block
-        :return: <str>
-        """
+        @staticmethod
+        def hash(block):
+          """
+          Creates an SHA-256 hash of a Block
 
-        #We must make sure that the dictionary is ordered, or we'll have inconsistent hashes
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
+          :param block: <dict> Block
+          :return: <str>
+          """
+
+          #We must make sure that the dictionary is ordered, or we'll have inconsistent hashes
+          block_string = json.dumps(block, sort_keys=True).encode()
+          return hashlib.sha256(block_string).hexdigest()
+
+        def proof_of_work(self, last_proof):
+          """
+          Simple proof of work algorithm:
+          - find a number p' such that hash({p}{p'}) constains 4 leading 0s, where p is 
+          the previous p'
+          - p is the previous proof, and p' is the new proof
+
+          :param last_proof: <int>
+          :return: <int>
+          """
+
+          proof = 0
+          while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+          return proof
+
+        def valid_proof(last_proof, proof):
+          """
+          Validates the proof: Does hash({last_proof}{proof}) contain 4 leading 0s?
+
+          :param: last_proof: <int> previous proof
+          :param: proof: <int> current proof
+          :return: <bool> true if correct, false if not
+          """  
+
+          guess = f'{last_proof}{proof}'.encode()
+          guess_hash = hashlib.sha256(guess).hexdigest()
+          return guess_hash[:4] == "0000"
+
+
+
+
+
+
+
+
+
+
+
 
